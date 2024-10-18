9c9d957f97e02a3417f8506c1dc5b191c92f4d29
afonseca00 <113251401+afonseca00@users.noreply.github.com>
2024-10-18 19:33:00 +0100

    quarto commit

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
-require('dotenv').config(); // Carrega variáveis de ambiente do arquivo .env
-const express = require('express'); // Framework web para Node.js
-const cors = require('cors'); // Middleware para habilitar CORS
-const path = require('path'); // Módulo para trabalhar com caminhos de arquivos
-const authRoutes = require('./routes/authRoutes'); // Rotas de autenticação
-const quizRoutes = require('./routes/quizRoutes'); // Rotas do quiz
-const errorMiddleware = require('./middleware/errorMiddleware'); // Middleware de tratamento de erros
-const db = require('./config/db'); // Conexão com o banco de dados
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+const quizRoutes = require('./routes/quizRoutes');
+const errorMiddleware = require('./middleware/errorMiddleware');
+const db = require('./config/db');
 
-const app = express(); // Cria uma instância do Express
+const app = express();
 
 // Middleware
-app.use(cors()); // Habilita CORS para todas as rotas
-app.use(express.json()); // Prepara parsing de JSON no corpo das requisições
-app.use(express.urlencoded({ extended: true })); // Prepara parsing de URL-encoded no corpo das requisições
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Servir arquivos estáticos da pasta 'public'
-app.use(express.static(path.join(__dirname, '../public')));
+// Servir arquivos estáticos da pasta 'frontend'
+app.use(express.static(path.join(__dirname, 'frontend')));
 
-// Rota raiz para servir o index.html
+// Rota raiz para servir o arquivo index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'index', 'index.html'));
 });
 
 // Rotas da API
@@ -32,7 +32,6 @@ app.use(errorMiddleware);
 // Inicia o servidor na porta especificada nas variáveis de ambiente ou 5000
 const PORT = process.env.PORT || 5000;
 
-// Verifica a conexão com o banco de dados antes de iniciar o servidor
 db.connect((err) => {
   if (err) {
     console.error('Erro ao conectar ao banco de dados:', err.message);
@@ -45,4 +44,4 @@ db.connect((err) => {
   }
 });
 
-module.exports = app; // Exporta a instância do Express para testes
+module.exports = app;
