13ff073d0f3f7565593d4b09b562d59911ec1345
afonseca00 <113251401+afonseca00@users.noreply.github.com>
2024-10-19 00:25:49 +0100

    Update login.js

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -78,7 +78,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         try {
             // Fazer uma requisição para o backend
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://quiz-game-rugby-ecdkbfh6ecgycybh.canadacentral-01.azurewebsites.net/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
